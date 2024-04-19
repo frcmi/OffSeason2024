686ae78601289eff081361cb73296cf17e4f77cc
Grant Lee <eeip200@gmail.com>
2024-04-18 17:15:46 -0700

    Did from scratch this time bc Nora mad :(
    honestly still looks the same but I understand what is happening now 👍

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,66 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.lib.ultralogger.UltraDoubleLog;
-import frc.lib.ultralogger.UltraTempLog;
-import frc.robot.Constants.ClimberConstants;
 
-public class ClimberSubsystem extends SubsystemBase {
-    // defines the two motors being used for the climbers
-    // id will change when robot ready unless they are same for some reason
-    private final CANSparkMax leftClimberMotor = new CANSparkMax(ClimberConstants.kLeftClimberId, MotorType.kBrushless);
-    private final CANSparkMax rightClimberMotor = new CANSparkMax(ClimberConstants.kRightClimberId, MotorType.kBrushless);
+public class Climber2Subsystem extends SubsystemBase {
+  /** Creates a new Climber2Subsystem. */
+  public Climber2Subsystem() {
 
-    private final UltraDoubleLog leftCurrentPublisher = new UltraDoubleLog("Climber/Left Motor Current");
-    private final UltraDoubleLog rightCurrentPublisher = new UltraDoubleLog("Climber/Right Motor Current");
-    private final UltraTempLog leftTemperaturePublisher = new UltraTempLog("Climber/Left Motor Temperature", leftClimberMotor::getMotorTemperature);
-    private final UltraTempLog rightTemperaturePublisher = new UltraTempLog("Climber/Right Motor Temperature", rightClimberMotor::getMotorTemperature);
+  }
 
-    public ClimberSubsystem() {
-        leftClimberMotor.setIdleMode(IdleMode.kBrake);
-        rightClimberMotor.setIdleMode(IdleMode.kBrake);
-
-        setDefaultCommand(stop());
-    }
-
-
-    @Override
-    public void periodic() {
-        leftCurrentPublisher.update(leftClimberMotor.getOutputCurrent());
-        rightCurrentPublisher.update(rightClimberMotor.getOutputCurrent());
-        leftTemperaturePublisher.update();
-        rightTemperaturePublisher.update();
-    }
-
-    public Command up() { //Subject to change due to motor shenanigans
-        return run (
-                () -> {leftClimberMotor.set(ClimberConstants.kClimberUp);
-                    rightClimberMotor.set(ClimberConstants.kClimberUp);
-                }
-        ).until(() -> leftClimberMotor.getOutputCurrent() > 35 || leftClimberMotor.getOutputCurrent() > 35).withName("up");
-    }
-
-    public Command down() { //Subject to change due to motor shenanigans
-        return run (
-                () -> {leftClimberMotor.set(ClimberConstants.kClimberDown);
-                    rightClimberMotor.set(ClimberConstants.kClimberDown);
-                }
-        ).withName("down");
-    }
-    
-    public Command stop() {
-        return run(
-            () -> {leftClimberMotor.set(0);
-                rightClimberMotor.set(0);
-                }
-        ).withName("stop");
-    }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
-
-
-
