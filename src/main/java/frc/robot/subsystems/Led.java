1891cfa67e628c23efbf15dfde867ee722740cb1
Aiden Chen <aiden.chen@misd400.org>
2024-04-22 17:06:59 -0700

    revised code

--- src/main/java/frc/robot/subsystems/Led.java
+++ src/main/java/frc/robot/subsystems/Led.java
@@ -1,12 +1,10 @@
 package frc.robot.subsystems;
 
-import org.ejml.dense.block.MatrixOps_DDRB;
-
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.Constants.LedConstants;; 
 
 
@@ -45,84 +43,66 @@ public class Led extends SubsystemBase{
     }
 
     //changes colour of all Led's to a certan RGB
-    public void setColourRGB(Color8Bit colour) { 
+    public void setColourRGB(Color colour) { 
         for (int i = 0; i < m_LedBuffer.getLength(); i++) {
-            m_LedBuffer.setRGB(i , colour.red, colour.green, colour.blue);
+            m_LedBuffer.setRGB(i , (int)(colour.red * 255), (int)(colour.green * 255), (int)(colour.blue * 255));
         }
         m_Led.setData(m_LedBuffer); 
     }
 
     
     public void setLEDRed() {
-        for (int i = 0; i < m_LedBuffer.getLength(); i++) { //:)
-            m_LedBuffer.setRGB(i , 255, 0, 0);
-        }
-        m_Led.setData(m_LedBuffer); 
+       setColourRGB(new Color(255,0,0));
     }
 
     public void setLEDBlue() {
-        for (int i = 0; i < m_LedBuffer.getLength(); i++) {
-            m_LedBuffer.setRGB(i , 0, 0, 255);
-        }
-        m_Led.setData(m_LedBuffer); 
-    }
-
-    public void setLEDMaroon() {
-        for (int i = 0; i < m_LedBuffer.getLength(); i++) {
-            m_LedBuffer.setRGB(i , 128, 0, 0);
-        }
-        m_Led.setData(m_LedBuffer); 
+        setColourRGB(new Color(0,0,255));
     }
 
     public void setLEDWhite() {
-        for (int i = 0; i < m_LedBuffer.getLength(); i++) {
-            m_LedBuffer.setRGB(i , 255, 255, 255);
-        }
-        m_Led.setData(m_LedBuffer); 
+        setColourRGB(new Color(255,255,255));
     }
 
     public void setLEDoff() {
-        for (int i = 0; i < m_LedBuffer.getLength(); i++) {
-            m_LedBuffer.setRGB(i , 0, 0, 0);
-        }
-        m_Led.setData(m_LedBuffer); 
+        setColourRGB(new Color());
     }
     
     //changes colour of all Led's to a certan HSV
     public void setColourHSV(int hue, int saturation, int value) { 
-        for (int i = 0; i < m_LedBuffer.getLength(); i++) {
-            m_LedBuffer.setHSV(i, hue, saturation, value);
-            }
-        m_Led.setData(m_LedBuffer); 
+        setColourRGB(Color.fromHSV(hue, saturation, value));
     }
 
     //Makes a pulse of a certian colour pulse across it 
     //Takes in an arrays in form {red value, green value, blue value}
-    public void pulseColour(Color8Bit baseColour, Color8Bit pulseColour, int width, int speed) {
+    public void pulseColour(Color baseColour, Color pulseColour, int width, int speed) {
         int leadingPixel = 0; 
 
         //change colour to base colour 
-        for (int i = 0; i < m_LedBuffer.getLength(); i++) {
-            m_LedBuffer.setRGB(i, baseColour.red, baseColour.blue, baseColour.green);
-            }
-         m_Led.setData(m_LedBuffer); 
+        setColourRGB(baseColour); 
     
         //doing the pulse 
         int startingPixel = leadingPixel - (width / 2); 
         for (int i = startingPixel; i < width; i++) {
-        if (leadingPixel > m_LedBuffer.getLength()) {break;}//:)
-        m_LedBuffer.setRGB(i, pulseColour.red, pulseColour.blue, pulseColour.green);
+        if (leadingPixel > m_LedBuffer.getLength()) {break;}
+        m_LedBuffer.setRGB(i, (int)(pulseColour.red * 255), (int)(pulseColour.green * 255), (int)(pulseColour.blue * 255));
         }
 
         //iterate 
         startingPixel += speed; 
     }
 
-    public void constantRGB() {
+    public void rainbow(int speed, int width) {
+        int leadingPixel = 0; 
+        int startingPixel = leadingPixel - (width /2); 
+        Color startingColour = 
 
+        for (int i = startingPixel; i < width; i++) {
+            if (leadingPixel > m_LedBuffer.getLength()) {break;}
+
+        }
+
+        startingPixel += speed; 
     }
-
-
 }
 
 
