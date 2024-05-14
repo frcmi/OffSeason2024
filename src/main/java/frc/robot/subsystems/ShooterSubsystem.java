9bc90dd4696a3849b495aac11db6d4e949a372de
Nora Beda <theo.github@bedafamily.com>
2024-05-14 16:19:24 -0700

    initial auto align implementation

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,10 +5,13 @@ import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.controller.ArmFeedforward;
 
+import java.util.Optional;
 import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -35,6 +38,44 @@ public class ShooterSubsystem extends SubsystemBase {
         shooterMotor.setInverted(false);
 
         setDefaultCommand(stop());
+    }
+
+    /**
+     * Computes the angle to shoot a ring at, not accounting for air resistance, but accounting for gravity
+     * @param shooterToTarget The vector from the shooter to the target in METERS (e.g. targetPose.minus(shooterPose))
+     * @param initialVelocity The velocity that the shooter would shoot at in METERS PER SECOND
+     * @return The angle to shoot at, if possible
+     */
+    public static Optional<Rotation2d> computeShooterAngle(Translation3d shooterToTarget, double initialVelocity) {
+        // 0 = -delta_y + v_not * t * sin(theta) - 1/2 * g * t^2
+        // t = (v_not * t * sin(theta) +/- sqrt(v_not^2 * sin^2(theta) - 2 * g *
+        // delta_y)) / g
+        // => v_not^2 * sin^2(theta) - 2 * g * delta_y >= 0
+        // v_not^2 * sin^2(theta) >= 2 * g * delta_y
+        // v_not * sin(theta) >= sqrt(2 * g * delta_y)
+        // sin(theta) >= 1/v_not * sqrt(2 * g * delta_y)
+        // theta >= asin(1/v_not * sqrt(2 * g * delta_y))
+
+        // -delta_y + v_not * t * sin(theta) - 1/2 * g * t^2 = -delta_x + v_not * t *
+        // cos(theta)
+        // delta_x - delta_y + v_not * t * (sin(theta) - cos(theta)) - 1/2 * g * t^2 = 0
+        // t = (v_not * (sin(theta) - cos(theta)) +/- sqrt(v_not^2 * (sin(theta) -
+        // cos(theta))^2 - 2 * g * (delta_y - delta_x)))/g
+        // => v_not^2 * (sin(theta) - cos(theta))^2 - 2 * g * (delta_y - delta_x) >= 0
+        // v_not^2 * (sin(theta) - cos(theta))^2 >= 2 * g * (delta_y - delta_x)
+        // v_not * (sin(theta) - cos(theta)) >= sqrt(2 * g * (delta_y - delta_x))
+        // sin(theta) - cos(theta) >= 1/v_not * sqrt(2 * g * (delta_y - delta_x))
+
+        double delta_x = shooterToTarget.toTranslation2d().getNorm();
+        double delta_y = shooterToTarget.getZ();
+        double g = 9.81;
+
+        double theta = Math.asin(1 / initialVelocity * Math.sqrt(2 * g * delta_y));
+        if (Math.sin(theta) - Math.cos(theta) < 1 / initialVelocity * Math.sqrt(2 * g * (delta_y - delta_x))) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new Rotation2d(theta));
     }
 
     public double getAngle() {
